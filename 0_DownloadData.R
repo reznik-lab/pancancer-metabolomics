15816015f5a9fed193b8648466cd06c8255a6ebb
elisa <elibene@gmail.com>
2022-11-23 13:52:40 -0800

    added code to automatically download precalculated data

--- 0_DownloadData.R
+++ 0_DownloadData.R
@@ -3,7 +3,7 @@
 # set working directory to source file location
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
-#### Download Data ----
+#### Download Molecular Data ----
 
 # download preprocessed data from Zotero
 temp <- tempfile()
@@ -18,3 +18,12 @@ newDir <- paste0(getwd(),"/.", sep="")
 R.utils::copyDirectory(oldDir, newDir)
 # remove folder
 unlink(paste0(getwd(),"/pancancer_metabolomics",sep=""), recursive = TRUE)
+
+#### Download Precalculated Data ----
+
+# download precomputed data from Zotero
+temp <- tempfile()
+download.file(url="https://zenodo.org/record/7352546/files/data_for_scripts_v0.3.3.tar.gz?download=1",
+              destfile = temp)
+# extract archive
+untar(tarfile=temp, exdir = ".")
